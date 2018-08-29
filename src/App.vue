fd85cd0507e60ef239a1fc7307055894e988af1b
Thomas Miller <tmi@macmac.ch>
2018-08-29 17:08:13 +0200

    setup, rdy for gsap

---
+++ src/App.vue
@@ -0,0 +1,170 @@
+<template>
+  <div id="app">
+    <div class="top-wrapper">
+      <div class="text">
+        <div class="main-text">
+          <h1 class="first">23. September</h1>
+          <h1 class="second">2x NEIN</h1>
+        </div>
+        <h2>zur Fair-Food-Initiative und Ernährungssouveränität</h2>
+      </div>
+      <div class="images">
+        <img class="korb" src="./assets/korb.png">
+        <div class="svg-wrapper">
+          <div class="svg-text">
+            <div class="sonst">sonst wirds</div>
+            <div class="teurer">TEURER!</div>
+          </div>
+          <svg version="1.1" id="blob" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
+            viewBox="0 0 1311.1 1311.2" style="enable-background:new 0 0 1311.1 1311.2;" xml:space="preserve">
+              <path d="M105.4,1018.4c1.4-6.4,2.3-12.9,4.3-19.1c7.1-21.4,14.6-42.7,21.8-64.1c10.8-32.1-4.7-65.1-36.1-78.1
+                c-21.4-8.8-42.6-18-63.7-27.4c-19.4-8.6-29-25.5-24-43.8c1.9-7,5.8-13.9,10.4-19.5c14.3-17.2,29.4-33.9,44.3-50.7
+                c24.8-28,23.8-62.9-2.6-89.6c-15.9-16.1-31.7-32.2-47.3-48.6c-19.1-20-15.7-48.9,7.1-61c13.7-7.3,27.6-14,41.4-20.9
+                c8.6-4.3,17.4-8.3,26.1-12.5c29.3-14.1,42.9-49,30.7-79.1c-8.8-21.8-17.6-43.5-26.2-65.4c-8-20.4-2.8-40,14.1-49.3
+                c6.6-3.6,14.7-5.7,22.3-6.4c24.2-2.2,48.5-2.6,72.6-5.2c31.7-3.4,53.9-29,54.5-61c0.4-23.2,0.4-46.3,0.8-69.5
+                c0.2-12,3.4-23.1,12.6-31.5c12.3-11.2,26.6-11.6,41.5-6.7c22.7,7.4,45.1,15.4,67.8,22.9c30,10,63.7-5.6,76-34.8
+                c8.7-20.6,17.3-41.3,26.1-61.9c4.1-9.5,9.3-18.2,18.6-23.7c14.5-8.6,31.6-6.5,46,5.9c17.4,15,34.6,30.2,51.8,45.5
+                c26.9,23.8,62.3,22.9,88-2.4c16.1-15.9,32.2-31.7,48.5-47.4C743.3,3,755.9-2.6,770.9,1.2c12.7,3.2,21,12.1,26.6,23.5
+                c10.1,20.4,20.2,40.8,29.9,61.4c15.4,32.6,53.3,43.7,81.2,31.3c20.4-9.1,41.5-16.7,62.3-25.1c10.3-4.1,20.8-5.7,31.5-2.3
+                c14.8,4.6,24.7,18.9,25.8,36.5c1.6,24.6,2.7,49.2,5,73.7c2.8,29.9,26.7,53,56.8,54.6c24,1.2,48.1,0.9,72.2,1.2
+                c14.5,0.2,27.5,4,36.3,16.5c8.4,12,7.8,25,3.4,38.2c-7.5,22.4-15.3,44.8-22.8,67.2c-9.5,28.6,4.2,61.3,31.8,74
+                c22.3,10.3,45.3,19.1,67.5,29.5c7,3.3,13.9,8.4,18.8,14.3c10.7,12.9,10.3,30.9-1,45c-13,16.2-27.1,31.6-40.8,47.2
+                c-9.4,10.7-19.2,21-22.5,35.6c-4.8,21.1-1.2,40.5,13.6,56.6c16.3,17.7,33.5,34.6,50.3,51.8c7.6,7.8,13.2,16.5,14,27.7
+                c1.1,15.9-5.9,27.9-19.5,35.2c-21.5,11.5-43.6,21.9-65.5,32.7c-26.5,13.1-40.5,37.5-36.1,66.5c1.6,10.8,6.7,21.1,10.7,31.4
+                c6.4,16.7,13.6,33,19.6,49.8c10,28-6.3,51.7-36.1,53.5c-25.1,1.6-50.3,2.9-75.3,5.2c-26.5,2.5-50.6,27.3-51.9,54
+                c-1.2,25.1-1,50.3-1.4,75.5c-0.2,12.3-3.1,23.6-12.4,32.2c-10.9,10.2-24.2,12.1-37.8,7.9c-23.5-7.3-46.5-15.7-69.9-23.4
+                c-24.9-8.2-46.7-2.3-65.3,15.7c-8.3,8.1-12.5,18.7-16.9,29.1c-8.2,19.5-16.3,39-25.1,58.2c-10.1,22.1-36.4,29.3-56.2,14.5
+                c-15.2-11.3-28.9-24.5-43.2-37c-5.4-4.7-10.6-9.5-16-14.1c-25.6-21.4-61-19.9-84.8,3.6c-16.2,16-32.6,31.9-48.9,47.8
+                c-7.2,7-15.5,12.3-25.7,13.3c-16.5,1.7-30.7-6.5-38.9-22.7c-11.4-22.6-21.9-45.6-33.9-67.9c-14.4-26.9-47.4-38.2-75.9-27
+                c-21.9,8.6-43.6,17.4-65.5,26.1c-11.8,4.6-23.7,6-35.4,0c-14.6-7.5-19.9-20.6-21-35.9c-1.8-25.1-2.8-50.3-5.4-75.3
+                c-2.7-26.7-27.7-50-54.6-51.3c-25-1.1-50-0.8-74.9-1.5c-5.9-0.2-12.1-1.1-17.6-3C115.3,1047.2,106.7,1034.4,105.4,1018.4"/>
+          </svg>
+        </div>
+      </div>
+    </div>
+    <div class="bottom">
+      <img class="logo" src="./assets/logo-kf.png">
+    </div>
+    <div class="website">agrarinitiativen-nein.ch</div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+};
+</script>
+
+<style lang="scss">
+@import '~@/resets.scss';
+body {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  background-color: #333;
+}
+#app {
+  width: 1920px;
+  height: 1080px;
+  padding: 100px;
+  background-color: white;
+  position: relative;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+.top-wrapper {
+  position: relative;
+  display: flex;
+  .text {
+    position: relative;
+    margin-top: 187px;
+  }
+  .images {
+    position: absolute;
+    right: 0px;
+    top: 50%;
+    transform: translateY(-50%);
+  }
+}
+h1 {
+  @include GeoBlack;
+}
+h1.first {
+  font-size: 100px;
+  color: red;
+  opacity: 0.1;
+  position: absolute;
+  top: -90px;
+}
+h1.second {
+  font-size: 280px;
+  line-height: 280px;
+  letter-spacing: 2px;
+}
+h2 {
+  @include GeoBlack;
+  font-size: 45px;
+  margin-top: -60px;
+}
+.korb {
+  margin-top: -100px;
+  width: 900px;
+}
+.svg-wrapper {
+  width: 350px;
+  position: absolute;
+  right: 0px;
+  bottom: 125px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  .svg-text {
+    margin-top: 20px;
+    transform: rotate(25deg);
+    transform-origin: center center;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    color: #F9ED67;
+    z-index: 1;
+    @include Radical;
+    text-transform: uppercase;
+    // margin-left: 48px;
+    .sonst {
+      font-size: 38px;
+    }
+    .teurer {
+      margin-top: 10px;
+      font-size: 62px;
+    }
+  }
+  #blob {
+    position: absolute;
+    path {
+      fill: #D9318A;
+    }
+  }
+}
+.bottom {
+  position: absolute;
+  bottom: 30px;
+  left: 50%;
+  transform: translateX(-50%);
+}
+.logo {
+  width: 300px;
+}
+.website {
+  font-size: 28px;
+  @include Radical;
+  color: #888;
+  position: absolute;
+  right: 40px;
+  top: 30px;
+  transform-origin: right;
+  transform: rotate(-90deg);
+}
+</style>
